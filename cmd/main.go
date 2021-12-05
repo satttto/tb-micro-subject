ced34cb8c12de2663c72d35e404998ac48f5b6d6
Satoshi Sugai <sensan.3104sato@gmail.com>
2021-12-05 22:49:37 +0900

    add: db

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,8 @@ import (
 	pb "github.com/satttto/tb-proto/subject"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 const (
@@ -22,12 +24,22 @@ func (s *server) ListSubject(ctx context.Context, in *pb.SubjectListRequest) (*p
 	log.Printf("Cursor: %v", in.GetCursor())
 	subjects := []*pb.Subject{
 		{Id: "1", Title: "title1", Members: []*pb.Member{{Id: "m1", Name: "Name"}}},
-		{Id: "2", Title: "title2", Members: []*pb.Member{{Id: "m1", Name: "Name"}}},
+		{Id: "2", Title: "title2", Members: []*pb.Member{{Id: "m2", Name: "Name"}}},
 	}
 	return &pb.SubjectListResponse{Subjects: subjects, Size: 12, HasNext: true}, nil
 }
 
 func main() {
+
+	dsn := "host=127.0.0.1 user=user password=password dbname=subject port=5433 sslmode=disable TimeZone=Asia/Tokyo"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln("接続失敗", err)
+	} else {
+		log.Println("hey")
+	}
+	log.Println(db)
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -39,4 +51,5 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/satttto/tb-micro-subject
 go 1.16
 
 require (
+	github.com/alexbrainman/sspi v0.0.0-20180613141037-e580b900e9f5 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
 	github.com/satttto/tb-proto v0.0.0-20211130120849-86e253d66d39
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/grpc v1.42.0
+	gopkg.in/jcmturner/aescts.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/dnsutils.v1 v1.0.1 // indirect
+	gopkg.in/jcmturner/goidentity.v3 v3.0.0 // indirect
+	gopkg.in/jcmturner/gokrb5.v7 v7.5.0 // indirect
+	gopkg.in/jcmturner/rpc.v1 v1.1.0 // indirect
+	gorm.io/driver/postgres v1.2.3 // indirect
+	gorm.io/gorm v1.22.4 // indirect
 )
